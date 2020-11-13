73bf210edf02884a0808a8b458b7c4ad2d19b41c
Igor Busto <igor.busto@edu.unifil.br>
2020-11-13 19:58:23 -0300

    Interpolacao Polinomial de lagrange finalizado

--- interpolacaoPolinomial/lagrange.py
+++ interpolacaoPolinomial/lagrange.py
@@ -1,13 +1,13 @@
 import PySimpleGUI as sg
-
+from itertools import product
 
 class lagrange:
     def __init__(self):
-        #sg.change_look_and_feel('LightBrown4')
         layout = [
             #[sg.Text('                                         Método de Newton', size = (50,0))],
             [sg.Text(' ')],
-            [sg.Text('lagrange:', size = (7,0))],
+            [sg.Text('f(x):', size = (3,0)), sg.Input(size = (15,0))],
+
             [sg.Output(size = (80,20))],
             [sg.Button('Executar'), sg.Quit('Sair')],
         ]
@@ -19,15 +19,54 @@ class lagrange:
     def Iniciar(self):
         while True:
             self.event, self.values = self.janela.Read()
+            fx = float(self.values[0])
+
+            x = ([-1,0,2])
+            y = ([4,1,-1])
+
+            print('x',x)
+            print('y',y)
+            
+            final = 0 
+            for j in range(0, len(x)):
+                L = 1
+                M = 1
+                for i in range(0, len(x)): 
+                    if(i != j):
+                        if(i == 0):
+                            L = (fx - x[i])
+                            print('cima  ', fx, '-', x[i], '=', L)
+                            M = (x[j] - x[i])
+                            print('baixo   ', x[j], '-', x[i], '=', M)
+                         
+                        else:
+                            L = L * (fx - x[i])  
+                            print('cima  ', fx, '-', x[i], '=', L)    
+                            M = M * (x[j] - x[i])
+                            print('baixo   ', x[j], '-', x[i], '=', M)
+                instancia = float(L / M)
+                print('resultado: ', instancia)
+                print()
+
+                final = y[j]*instancia + final
+            print('Resultado final: ', final)
+                
+
+
+                    
+        
+            
     
             if self.event in (None, 'Sair'):
                 break
-            #if self.event == 'Executar':
-                #self.metodoNewton(formula, xZero, e)
 
         self.janela.Close()
         sg.popup_ok('Interpolação Polinomial de Lagrange finalizado!')
           
+    def execLagrange(self, x, fx):                                                             
+        L = lambda num, xi: product((num - xj) / (xi - xj) for xj in x if xj != xi)                                                                               
+        return lambda num: sum([yi * L(num, xi) for xi, yi in zip(x, fx)])
+
 
 tela = lagrange()
 tela.Iniciar()
